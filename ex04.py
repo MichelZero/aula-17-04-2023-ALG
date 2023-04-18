02d2ec599f52431c007e1a0da83a1fc666dedde2
Michel <106674127+MichelZero@users.noreply.github.com>
2023-04-18 12:00:49 -0300

    up

---
+++ ex04.py
@@ -0,0 +1,14 @@
+#
+# autores:
+# Cristiano e Michel
+
+# data: 17/04/2023
+
+# Escreva um programa que verifica se uma pessoa pode dirigir ou 
+# não com base em sua idade e imprime a mensagem correspondente.
+
+idade = int(input("Digite sua idade: "))
+if idade >= 18:
+    print("Você pode dirigir.")
+else:
+    print("Você ainda não pode dirigir.")
