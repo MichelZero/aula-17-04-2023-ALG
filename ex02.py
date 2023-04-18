a1973073dd92edc00a1bbbe59954885627438054
Michel <106674127+MichelZero@users.noreply.github.com>
2023-04-18 11:58:43 -0300

    up

---
+++ ex02.py
@@ -0,0 +1,14 @@
+#
+# autores:
+# Cristiano e Michel
+
+# data: 17/04/2023
+
+# Escreva um programa que verifica se um número é par ou ímpar 
+# e imprime a mensagem correspondente.
+
+numero = int(input("Digite um número: "))
+if numero % 2 == 0:
+    print("O número é par.")
+else:
+    print("O número é ímpar.")
